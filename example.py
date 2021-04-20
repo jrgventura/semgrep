fa765a2f41c78f90e1c445d15ec2fc5596455632
Jorge Ventura <jorge.venturag@outlook.com>
2021-04-20 01:13:16 -0500

    demo

--- example.py
+++ example.py
@@ -15,3 +15,10 @@ sol1 = (-b-cmath.sqrt(d))/(2*a)
 sol2 = (-b+cmath.sqrt(d))/(2*a)
 
 print('The solution are {0} and {1}'.format(sol1,sol2))
+
+def hello_world(abc):
+    logger.info('starting skynet')
+    skynet.init()
+    # TODO Change this to logging framework before prod
+    print(f'--> debug, skynet init vector is {skynet.iv}')
+    return skynet.rule_forever()
